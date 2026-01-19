2ae41b36872db00d83b64828805b99e8a1a6ebe1
caoliangqiang <clq152822>
2026-01-19 15:56:55 +0800

    导入导出调整

--- backend/src/main/java/com/app/novelvoice/NovelVoiceApplication.java
+++ backend/src/main/java/com/app/novelvoice/NovelVoiceApplication.java
@@ -1,13 +1,15 @@
 package com.app.novelvoice;
 
+import com.gmrfid.excel.annotation.EnableExcel;
+import com.gmrfid.file.storage.annotation.EnableFileStorage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.app.novelvoice.mapper")
-@com.gmrfid.file.storage.annotation.EnableFileStorage
-@com.gmrfid.excel.annotation.EnableExcel
+@EnableFileStorage
+@EnableExcel
 public class NovelVoiceApplication {
 
     public static void main(String[] args) {
